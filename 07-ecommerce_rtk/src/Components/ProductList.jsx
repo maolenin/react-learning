ff844cf631f89213ecbd7f39d896aea540829f2a
Mao Soldevilla <maolenin@gmail.com>
2025-06-02 21:44:43 -0500

    Updated 07 excercise

--- 07-ecommerce_rtk/src/Components/ProductList.jsx
+++ 07-ecommerce_rtk/src/Components/ProductList.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import './ProductList.css'; 
+import './ProductList.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItemToCart } from './CartSlice'; // Action to add product to cart
 
 const ProductList = () => {
+
+  const dispatch = useDispatch();
+  const cartItems = useSelector(state => state.cart.cartItems); // Get cart items globally
+  const handleAddToCart = product => {
+      dispatch(addItemToCart(product)); // add product to cart
+      };
 
   const products = [
     { id: 1, name: 'Product A', price: 60 },
@@ -13,7 +21,22 @@ const ProductList = () => {
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
-     
+        {products.map((product) => {
+          const isAlreadyInCart = cartItems.some(item => item.id === product.id);
+
+          return (
+            <li key={product.id} className="product-list-item">
+              <span>{product.name} - ${product.price}</span>
+              <button
+                className={`add-to-cart-btn ${isAlreadyInCart ? 'disabled' : ''}`}
+                onclick={() => handleAddToCart(product)}
+                disabled={isAlreadyInCart}
+                >
+                {isAlreadyInCart ? 'added' : 'Add to Cart'}
+            </button>
+          </li>
+        );
+         })}
       </ul>
     </div>
   );
--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 
 ## Clone new project into react-learning repo
 ### Steps to start a new computer and sync with the existing work
+
+1. Clone the repository
+```
 git clone https://github.com/YOUR_USERNAME/YOUR_REPO.git
 cd YOUR_REPO
+```
 
 ### Add external repo into an existing repo
-```git remote add external https://github.com/EXTERNAL_USER/EXTERNAL_REPO.git
+```
+git remote add external https://github.com/EXTERNAL_USER/EXTERNAL_REPO.git
 git fetch external
-
 git read-tree --prefix=external_code/ -u external/main
 git commit -m "Imported external repo into external_code folder"
 git push origin main
